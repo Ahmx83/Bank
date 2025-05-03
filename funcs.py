f2a129ee6ee753ed4c82136d5774d3b9375171c5
Ahmed Abdelgadir <ahmedyazeid7@gmail.com>
2025-05-03 14:10:15 +0300

    Added getAccountNo(), and getPin() to func.py.

--- funcs.py
+++ funcs.py
@@ -1,17 +1,45 @@
 from typing import TypeVar
+from bank import checkValidPin, Account, checkValidAccountNo
 
 T = TypeVar('T')
 
-def getValidInput(invalid_msg: str, *expected: T, key = None) -> T:
+def getValidInput(invalid_msg: str, *expected, key = None) -> str:
     """Keeps asking for input until given an expected input."""
-    user_input = input("> ")
-    if key:
-        user_input = key(user_input)
+    if key is None:
+        key = lambda x: x
 
-    while user_input not in expected:
+    user_input = input("> ")
+    transformed_input = key(user_input)
+
+    while transformed_input not in expected:
         print(invalid_msg)
         user_input = input("> ")
-        if key:
-            user_input = key(user_input)
+        transformed_input = key(user_input)
 
     return user_input
+
+def getAccountNo() -> int:
+    """Returns a valid, existing account number."""
+    valid = False
+    account_no = 0
+    while not valid:
+        account_no = getValidInput("Account numbers are 9 digits. Try again.",
+                      9, key=len)
+        account_no = int(account_no)
+        valid = checkValidAccountNo(account_no)
+        if not valid:
+            print("You've entered an invalid account number.")
+    return account_no
+
+def getPin(account_no: int) -> int:
+    """Returns the right pin relative to the account_no given.
+    Assumes the given account_no is valid."""
+    valid = 0
+    pin = 0
+    while not valid:
+        pin = getValidInput("Pins are 4 digits. Try again.",
+                            4, key=len)
+        pin = int(pin)
+        valid = checkValidPin(account_no, pin)
+        print(valid)
+    return pin
