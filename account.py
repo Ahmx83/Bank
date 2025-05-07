0a4aa68e7f65443257597ad2e5f06564b867fee0
Ahmed Abdelgadir <ahmedyazeid7@gmail.com>
2025-05-07 15:16:32 +0300

    renamed bank.py to account.py, moved Operators() class and some functions into Account().

--- account.py
+++ account.py
@@ -1,80 +1,79 @@
 import json
-from typing import Literal, Union
+from typing import Union
 import random
 
+# TODO: combine Account() & Operators()
 # TODO: Card() class
 
 # Account Classes =================================================================================
+class AccountException(Exception):
+    """This account module raises this when the module is misused."""
+    pass
+
 class Account:
     """Creates a new account"""
     def __init__(self, name: str, account_no: int, pin: int, balance: float):
         self.name = name.capitalize()
         self.account_no = account_no
         self.pin = pin
-        self.balance = balance
+        self._balance = balance
+
+    @property
+    def balance(self):
+        return self._balance
 
     def __str__(self):
-        return f"""{self.name}'s balance: {self.balance}"""
+        return f"""{self.name}'s _balance: {self._balance}"""
+
+    def withdraw(self, amount:float) -> float:
+        """Subtracts the amount from balance and returns the new balance."""
+        if amount > self.balance:
+            raise AccountException(
+            "The amount you are trying to withdraw is greater than your current balance."
+            )
+        self._balance -= amount
+        self._update_ledger()
+        return self._balance
+
+    def deposit(self, amount:float) -> float:
+        """Adds the amount to balance and returns the new balance."""
+        self._balance += amount
+        self._update_ledger()
+        return self._balance
+
+    @staticmethod
+    def _readLedgers() -> dict:
+        """Returns the contents of '_ledgers.json'"""
+        with open("_ledgers.json", "r+") as fp:
+            customers = json.load(fp)
+        return customers
+
+    def _update_ledger(self) -> None:
+        """Updates the details of the account in '_ledgers.json'."""
+        # Gets the current ledgers
+        customers = self._readLedgers()
+        # Make the change.
+        customers[str(self.account_no)].update(self.__dict__)
+        # Updates the ledgers
+        with open("_ledgers.json", "w") as fp:
+            json.dump(customers, fp, indent=2)  # type: ignore
+
 
 class NewAccount(Account):
     def __init__(self, name: str, pin: int):
         self.account_no = generateAccountNumber()
         super().__init__(name, self.account_no, pin, 0)
-        _update_ledger(self)
+        self._update_ledger()
 
 class ExistingAccount(Account):
     def __init__(self, account_no: int, pin: int):
-        customer = _readLedgers()[str(account_no)]
+        customer = self._readLedgers()[str(account_no)]
         name = customer["name"]
-        balance = customer["balance"]
+        balance = customer["_balance"]
         super().__init__(name, account_no, pin, balance)
 
 
-
-
-# Transactions ====================================================================================
-# TODO: Overload Operations() with arithmetic operators.
-class Operations:
-    """Does different operations on the account given in initiation."""
-    def __init__(self, account:Account):
-        self.account = account
-
-    def checkBalance(self) -> float:
-        """Returns the balance in account"""
-        with open("_ledgers.json", "r") as fp:
-            customers = json.load(fp)
-        return customers[self.account.account_no]["balance"]
-
-    def withdraw(self, amount:float) -> float:
-        """Subtracts the amount from balance and returns the new balance."""
-        self.account.balance -= amount
-        _update_ledger(self.account)
-        return self.account.balance
-
-    def deposit(self, amount:float) -> float:
-        """Adds the amount to balance and returns the new balance."""
-        self.account.balance += amount
-        _update_ledger(self.account)
-        return self.account.balance
-
-
 # Functions =======================================================================================
-def _readLedgers() -> dict:
-    """Returns the contents of '_ledgers.json'"""
-    with open("_ledgers.json", "r+") as fp:
-        customers = json.load(fp)
-    return customers
-
-def _update_ledger(account:Account) -> None:
-    """Updates the details of the given account in '_ledgers.json'."""
-    # Gets the current ledgers
-    customers = _readLedgers()
-    # Make the change.
-    customers[account.account_no] = account.__dict__
-    # Updates the ledgers
-    with open("_ledgers.json", "w") as fp:
-        json.dump(customers, fp, indent=2)  # type: ignore
-
 def generateAccountNumber() -> int:
     """Generates and returns new and original a 9-digit account number,
     and adds it to 'account_numbers.json'"""
@@ -114,4 +113,3 @@ def checkPin(account_no: Union[int|str],  pin: int) -> bool:
     acc = customers[str(account_no)]
     validPin = acc["pin"]
     return True if pin == validPin else False
-
